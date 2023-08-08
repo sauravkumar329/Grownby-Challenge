00814d1c20abf842f8c92374f4ccaef7d0faf192
sauravkumar329 <46621822+sauravkumar329@users.noreply.github.com>
2023-08-08 11:58:59 -0700

    updating gitignore

--- App.tsx
+++ App.tsx
@@ -1,4 +1,3 @@
-import { StyleSheet } from 'react-native';
 import React, { useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import AuthNavigator from './components/navigation/AuthNavigator';
